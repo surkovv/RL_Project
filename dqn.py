a13e57230feb70a909ea25283606df4dd7c92c50
fh1628 <faridahaddad13@gmail.com>
2025-05-19 22:20:01 +0200

    dqn evaluation metrics

--- dqn.py
+++ dqn.py
@@ -32,6 +32,7 @@ class DQNAgent:
             hidden_dim=128, episodes=600, 
             batch_size=64, num_steps=200, num_layers=2, 
             learning_rate=1e-3, eval_interval=5, epsilon=1.0,
+            tau = 0.05, gamma = 0.99,
             epsilon_min=0.05, epsilon_decay=0.99):
         self.env = gym.make(env_name)
         state_dim = self.env.observation_space.shape[0]
@@ -48,16 +49,16 @@ class DQNAgent:
         self.optimizer = optim.Adam(self.q_network.parameters(), lr=learning_rate)
         self.criteria = nn.MSELoss()
         self.action_dim = action_dim
-        self.gamma = 0.99
-        self.tau = 0.05
+        self.gamma = gamma
+        self.tau = tau
 
         self.epsilon = epsilon
         self.epsilon_min = epsilon_min
         self.epsilon_decay = epsilon_decay
         self.eval_interval = eval_interval
 
-    def select_action(self, state, eps_greedy=True):
-        if eps_greedy and random.random() < self.epsilon:
+    def select_action(self, state, deterministic=False):
+        if not deterministic and random.random() < self.epsilon:
             return random.randint(0, self.action_dim - 1)
         else:
             with torch.no_grad():
@@ -124,29 +125,56 @@ class DQNAgent:
 
             self.update_epsilon()
             rewards_history.append(total_reward)
-            print(f"Episode {ep}, Reward: {total_reward:.1f}, Epsilon: {self.epsilon:.2f}")
+            # print(f"Episode {ep}, Reward: {total_reward:.1f}, Epsilon: {self.epsilon:.2f}")
 
             if ep % self.eval_interval == self.eval_interval - 1:
-                state, _ = self.env.reset()
-                total_reward = 0
-                for n_step in range(self.num_steps):
-                    action = self.select_action(state, eps_greedy=False)
-                    next_state, reward, terminated, truncated, _ = self.env.step(action)
-                    done = terminated or truncated
-                    state = next_state
-                    total_reward += reward
-                    if done:
-                        break
+                eval_rewards = []
+                for _ in range(5):
+                    state, _ = self.env.reset()
+                    total_reward = 0
+                    for n_step in range(self.num_steps):
+                        action = self.select_action(state, deterministic=True)
+                        next_state, reward, terminated, truncated, _ = self.env.step(action)
+                        done = terminated or truncated
+                        state = next_state
+                        total_reward += reward
+                        if done:
+                            break
+                    eval_rewards.append(total_reward)
+                eval_rewards_history.append(np.mean(eval_rewards))
+                # print(f"Episode {ep}, Eval return: {total_reward:.2f}")
 
-                print(f"Episode {ep}, Eval return: {total_reward:.2f}")
-                eval_rewards_history.append(total_reward)
-
-        # Plotting
-        plt.plot(rewards_history)
-        plt.xlabel("Episode")
-        plt.ylabel("Reward")
-        plt.title("DQN on CartPole-v1")
-        plt.grid()
-        plt.show()
-
+        # # Plotting
+        # plt.plot(rewards_history)
+        # plt.xlabel("Episode")
+        # plt.ylabel("Reward")
+        # plt.title("DQN on CartPole-v1")
+        # plt.grid()
+        # plt.show()
         return rewards_history, eval_rewards_history
+
+    def evaluate_policy(self, num_episodes=20):
+        rewards = []
+        self.env.reset()
+
+        for ep in range(num_episodes):
+            state, _ = self.env.reset()
+            done = False
+            total_reward = 0.0
+
+            while not done:
+                action = self.select_action(state, deterministic=True)
+                next_state, reward, terminated, truncated, _ = self.env.step(
+                    action)
+                total_reward += reward
+                done = terminated or truncated
+                state = next_state
+
+            print(f"Episode {ep}, Eval return: {total_reward:.2f}")
+
+            rewards.append(total_reward)
+
+        mean_reward = np.mean(rewards)
+        std_reward = np.std(rewards)
+
+        return mean_reward, std_reward
